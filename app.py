af6497021b602b06896c50f774a28bfad4676dd7
Anshul <73289622+anshultaneja@users.noreply.github.com>
2021-06-15 12:16:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,19 +36,14 @@ print('Model loaded. Start serving...')
 
 
 def model_predict(img_path, model):
-    img = image.load_img(img_path, target_size=(150, 150,1)) #target_size must agree with what the trained model expects!!
+     img = image.load_img(img_path, target_size=(150,150,1)) #target_size must agree with what the trained model expects!!
 
-#     # Preprocessing the image
-#     img = image.img_to_array(img)
-#     img = np.expand_dims(img, axis=-1)
+    # Preprocessing the image
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
 
    
-#     preds = model.predict(img)
-    image = Image.open(img_path)
-    image = image.resize((150,150))
-    x = np.array(image)
-    preds = model.predict(x)
-    preds=int(np.argmax(preds,axis =-1))
+    preds = model.predict(img)
     return preds
 
 
