4d8d961f010d2f90234dcc4b4ee6fd8e7f07f84c
Anshul <73289622+anshultaneja@users.noreply.github.com>
2021-06-15 12:40:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-﻿from __future__ import division, print_function
+from __future__ import division, print_function
 # coding=utf-8
 import sys
 import os
@@ -7,15 +7,14 @@ import re
 import numpy as np
 
 # Keras
-
 from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
-from PIL import Image
+
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-# from gevent.wsgi import WSGIServer
+from gevent.wsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
@@ -28,55 +27,53 @@ model = load_model(MODEL_PATH)
 #model._make_predict_function()          # Necessary to make everything ready to run on the GPU ahead of time
 print('Model loaded. Start serving...')
 
-# You can also use pretrained model from Keras
-# Check https://keras.io/applications/
-#from keras.applications.resnet50 import ResNet50
-#model = ResNet50(weights='imagenet')
-#print('Model loaded. Check http://127.0.0.1:5000/')
+
 
 
 def model_predict(img_path, model):
-	
-    	img = image.load_img(img_path, target_size=(150,150,1)) #target_size must agree with what the trained model expects!!
+    img = image.load_img(img_path, target_size=(150, 150)) #target_size must agree with what the trained model expects!!
 
     # Preprocessing the image
-    	img = image.img_to_array(img)
-    	img = np.expand_dims(img, axis=0)
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
 
    
-    	preds = model.predict(img)
-    	return preds
+    preds = model.predict(img)
+    return preds
 
 
 @app.route('/', methods=['GET'])
-def index():	
+def index():
     # Main page
-    	return render_template('index.html')
+    return render_template('index.html')
 
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-	if request.method == 'POST':
-		# Get the file from post request
-		f = request.files['file']
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
 
-		basepath = os.path.dirname(__file__)
-		file_path = os.path.join(
-		    basepath,  secure_filename(f.filename))
-		f.save(file_path)
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
 
-		# Make prediction
-		preds = model_predict(file_path, model)
-		result=preds
-		
-        	if preds == 1:
-            		return 'Pneumonia'
-        	else:
-            		return 'Normal'
-		return result
-	return None
-	
+        # Make prediction
+        preds = model_predict(file_path, model)
+        os.remove(file_path)#removes file from the server after prediction has been returned
+
+        # Arrange the correct return according to the model. 
+		# In this model 1 is Pneumonia and 0 is Normal.
+        str1 = 'Pneumonia'
+        str2 = 'Normal'
+        if preds == 1:
+            return str1
+        else:
+            return str2
+    return None
+
     #this section is used by gunicorn to serve the app on Heroku
 if __name__ == '__main__':
         app.run()
-    
