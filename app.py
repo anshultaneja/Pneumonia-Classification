07d463a81cabd3dbe338321b2fcd2b8dcb989e6c
Anshul <73289622+anshultaneja@users.noreply.github.com>
2021-06-17 11:44:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ import re
 import numpy as np
 
 # Keras
-from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing import image
-from PIL import Image
+from keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from keras.models import load_model
+from keras.preprocessing import image
+
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-
+from gevent.wsgi import WSGIServer
 
 # Define a flask app
 app = Flask(__name__)
@@ -27,22 +27,23 @@ model = load_model(MODEL_PATH)
 #model._make_predict_function()          # Necessary to make everything ready to run on the GPU ahead of time
 print('Model loaded. Start serving...')
 
-
+# You can also use pretrained model from Keras
+# Check https://keras.io/applications/
+#from keras.applications.resnet50 import ResNet50
+#model = ResNet50(weights='imagenet')
+#print('Model loaded. Check http://127.0.0.1:5000/')
 
 
 def model_predict(img_path, model):
-    image = Image.open(img_path)
-    img = image.resize((64,64))
-    img = np.asarray(img).reshape((1, 64, 64, 1))/255
-    prediction = np.squeeze(model.predict(img))
-    if prediction < 0.5:
-        prediction = 0
-    else:
-        prediction = 1
-    
-    classes = {0:"Normal",1:"Pneumonia"}
-    
-    return classes[(prediction)]
+    img = image.load_img(img_path, target_size=(64, 64)) #target_size must agree with what the trained model expects!!
+
+    # Preprocessing the image
+    img = image.img_to_array(img)
+    img = np.expand_dims(img, axis=0)
+
+   
+    preds = model.predict(img)
+    return preds
 
 
 @app.route('/', methods=['GET'])
@@ -53,7 +54,6 @@ def index():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
-	
     if request.method == 'POST':
         # Get the file from post request
         f = request.files['file']
@@ -67,9 +67,21 @@ def upload():
         # Make prediction
         preds = model_predict(file_path, model)
         os.remove(file_path)#removes file from the server after prediction has been returned
-        return preds
+
+        # Arrange the correct return according to the model. 
+		# In this model 1 is Pneumonia and 0 is Normal.
+        str1 = 'Pneumonia'
+        str2 = 'Normal'
+        if preds == 1:
+            return str1
+        else:
+            return str2
     return None
 
     #this section is used by gunicorn to serve the app on Heroku
 if __name__ == '__main__':
         app.run()
+    #uncomment this section to serve the app locally with gevent at:  http://localhost:5000
+    # Serve the app with gevent 
+    #http_server = WSGIServer(('', 5000), app)
+    #http_server.serve_forever()
